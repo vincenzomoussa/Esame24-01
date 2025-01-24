12ad31e0131dc6d87e79e9029add8bba9a7d8780
Vincenzo Moussa <vincenzomoussa@gmail.com>
2025-01-24 12:24:41 +0100

    Collegato con API e BEARER in tutti i js, dato titolo agli html

---
+++ assets/js/backoffice.js
@@ -0,0 +1,3 @@
+const apiKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzkzNzdiZmYyMDE4YzAwMTU5MDc0NDciLCJpYXQiOjE3Mzc3MTc2OTUsImV4cCI6MTczODkyNzI5NX0.jk5xjJv1mlICSGtd1fZV0skqJntJz_evqWR_R4S9zdE";
+const url = "https://striveschool-api.herokuapp.com/api/product/";
