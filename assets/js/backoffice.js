e1933e2bfd414007c207d4e83d800df32a6e4639
Vincenzo Moussa <vincenzomoussa@gmail.com>
2025-01-24 17:28:07 +0100

    Implementato carrello anche nel backoffice, con relativo codice dedicato alla logica

--- assets/js/backoffice.js
+++ assets/js/backoffice.js
@@ -174,3 +174,56 @@ form.addEventListener("submit", (e) => {
       alert("Assenza di connessione...");
     });
 });
+
+// Ho implementato il carrello anche nella pagina backoffice, solo per una questione di linearità tra le diverse pagine.
+
+const key = "pokèmons";
+let cart = JSON.parse(localStorage.getItem(key));
+const newShoppingCart = function () {
+  const shoppingCartDiv = document.getElementById("shopping-cart");
+  shoppingCartDiv.innerText = "";
+  const cart = JSON.parse(localStorage.getItem(key));
+  if (cart) {
+    for (let i = 0; i < cart.length; i++) {
+      // Creo dei contenitori all'interno dell'offcanvas, quindi al momento dell'inserimento di qualche prodotto, il carrello si popola di quest'ultimi calcolando anche il totale di tali prodotti.
+      const newItem = document.createElement("div");
+      newItem.classList.add("d-flex", "justify-content-between");
+      newItem.innerHTML = `
+      <p class="mb-0">${cart[i].name}</p>
+      <div class="d-flex mb-3">
+        <p class="text-nowrap mb-0">€ ${cart[i].price}</p>
+        <button class="border border-0 bg-white delete"><i class="fas fa-trash-alt"></i></button>
+      </div>`;
+      shoppingCartDiv.appendChild(newItem);
+
+      const button = document.getElementsByClassName("delete")[i];
+      button.addEventListener("click", () => {
+        // Il bottone accanto al prodotto nel carrello (la spazzatura), lo elimina dal carrello stesso.
+        cart.splice(i, 1);
+        localStorage.setItem(key, JSON.stringify(cart));
+        newShoppingCart();
+      });
+
+      totalPrice = 0;
+      if (localStorage.getItem(key)) {
+        let partial = JSON.parse(localStorage.getItem(key));
+        for (let i = 0; i < partial.length; i++) {
+          totalPrice += Number(partial[i].price);
+          //  Calcolo del prezzo aggiornato ad ogni cambiamento del carrello, al singolo inserimento o eliminazione di prodotti.
+        }
+      }
+      const total = document.getElementById("total");
+      total.innerText = `€ ${totalPrice.toFixed(2)}`;
+    }
+  } else {
+    const total = document.getElementById("total");
+    total.innerText = "€ 0";
+  }
+};
+
+if (cart) {
+} else {
+  cart = [];
+}
+
+newShoppingCart();
